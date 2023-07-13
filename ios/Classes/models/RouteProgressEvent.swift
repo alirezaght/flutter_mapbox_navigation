62cbd45247200d29a56d80c5e2512ec30fed9e57
Cross <alirezaght@gmail.com>
2023-07-13 14:06:20 +0200

    Update RouteProgressEvent.swift

--- ios/Classes/models/RouteProgressEvent.swift
+++ ios/Classes/models/RouteProgressEvent.swift
@@ -5,44 +5,17 @@ import MapboxCoreNavigation
 
 public class MapBoxRouteProgressEvent : Codable
 {
-    let arrived: Bool
+    let currentLeg: MapBoxRouteLeg
     let distance: Double
     let duration: Double
-    let distanceTraveled: Double
-    let currentLegDistanceTraveled: Double
-    let currentLegDistanceRemaining: Double
-    let currentStepInstruction: String
-    let legIndex: Int
-    let stepIndex: Int
-    let currentLeg: RouteLeg
-    var priorLeg: RouteLeg? = nil
-    var remainingLegs: [RouteLeg] = []
-    var currentRouteIndex: Int = 0
 
     init(progress: RouteProgress) {
-
-        arrived = progress.isFinalLeg && progress.currentLegProgress.userHasArrivedAtWaypoint
+        
         distance = progress.distanceRemaining
-        distanceTraveled = progress.distanceTraveled
         duration = progress.durationRemaining
-        legIndex = progress.legIndex
-        stepIndex = progress.currentLegProgress.stepIndex
         
-        currentLeg = progress.currentLeg
-        
-        if(progress.priorLeg != nil)
-        {
-            priorLeg = progress.priorLeg!
-        }
-
-        for leg in progress.remainingLegs
-        {
-            remainingLegs.append(leg)
-        }
-
-        currentLegDistanceTraveled = progress.currentLegProgress.distanceTraveled
-        currentLegDistanceRemaining = progress.currentLegProgress.distanceRemaining
-        currentStepInstruction = progress.currentLegProgress.currentStep.description
+        currentLeg = MapBoxRouteLeg(leg: progress.currentLeg, progress: progress)
+                
     }
 
 
