59b768aee0d2885d433ce0cffc336c6b9cdfc6c1
Cross <alirezaght@gmail.com>
2023-07-27 17:16:08 +0200

    Update RouteEvent.swift

--- ios/Classes/models/RouteEvent.swift
+++ ios/Classes/models/RouteEvent.swift
@@ -1,4 +1,7 @@
 import Foundation
+import MapboxDirections
+import MapboxNavigation
+import MapboxCoreNavigation
 
 public class MapBoxRouteEvent : Codable
 {
@@ -8,5 +11,33 @@ public class MapBoxRouteEvent : Codable
     init(eventType: MapBoxEventType, data: String) {
         self.eventType = eventType
         self.data = data
+    }
+}
+
+
+public class MapBoxRoute : Codable {
+    let routeIndex: String
+    let distance: Double
+    let duration: Double
+    var legs: [MapBoxRouteLeg] = []
+    
+    init(route: IndexedRouteResponse) {
+        self.routeIndex = String(route.routeIndex)
+        self.distance = route.currentRoute!.distance
+        self.duration = route.currentRoute!.expectedTravelTime
+        self.legs = []
+        route.currentRoute?.legs.forEach({ leg in
+            self.legs.append(MapBoxRouteLeg(leg: leg, progress: nil))
+        })
+    }
+    
+    init(index: Int, route: Route) {
+        self.routeIndex = String(index)
+        self.distance = route.distance
+        self.duration = route.expectedTravelTime
+        self.legs = []
+        route.legs.forEach({ leg in
+            self.legs.append(MapBoxRouteLeg(leg: leg, progress: nil))
+        })
     }
 }
